a853c4bd4f9e6911d5e97d1a965d165792f67e9c
Nathan Hare <nhare330@gmail.com>
2018-01-12 06:44:29 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# RPG Blog
+
+This is the code used to make my personal blog. This is my firt full foray into
+Django, so I will be using this to help me learn the framework better.
---
+++ apps/blog/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, null=True)
+    body = models.CharField(max_length=5000)
+    tags = models.ManyToManyField(Tag, blank=True, null=True)
+    banner = models.CharField(max_length=2000, blank=True, null=True)
+    user = models.ForeignKey(User, models.PROTECT)
+    edit_date = models.DateTimeField(auto_now=True)
+    post_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
