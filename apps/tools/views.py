446e7d5690dea50e9a5b69b4314a6eed1f8bb3d7
Nathan Hare <nhare330@gmail.com>
2018-01-15 03:39:45 -0500

    Update Storygen

--- apps/tools/views.py
+++ apps/tools/views.py
@@ -1,11 +1,18 @@
+from django.conf import settings
 from django.shortcuts import render
+import os
 
 # Create your views here.
 def umdaar(request):
     return render(request, 'tools/umdaar.html')
 
 def storygen(request):
-    return render(request, 'tools/storygen.html')
+    root_dir = os.path.dirname(settings.BASE_DIR)
+    icon_dir = os.path.join(root_dir, 'apps/tools/static/tools/images/storygen')
+    icons = os.listdir(icon_dir)
+    print(icons)
+    print('poop')
+    return render(request, 'tools/storygen.html', {'icons': icons})
 
 def fate_charsheet(request):
     return render(request, 'tools/fate-charsheet.html')
