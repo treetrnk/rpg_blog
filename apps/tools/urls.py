0ebb81d81097437a395f3b9529761414712a0143
Nathan Hare <nhare330@gmail.com>
2018-01-14 20:01:38 -0500

    Add storygen and clean up links for move to production

--- apps/tools/urls.py
+++ apps/tools/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     url('mastersofumdaar/', views.umdaar, name='umdaar'),
+    url('storygen/', views.storygen, name='storygen'),
+    url('fate-charsheet/', views.fate_charsheet, name='fate_charsheet'),
 ]
 
--- apps/tools/views.py
+++ apps/tools/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def umdaar(request):
     return render(request, 'tools/umdaar.html')
+
+def storygen(request):
+    return render(request, 'tools/storygen.html')
+
+def fate_charsheet(request):
+    return render(request, 'tools/fate-charsheet.html')
