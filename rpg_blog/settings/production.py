c285845a45503dc6670475b9b8259c25966e649b
Nathan Hare <nhare330@gmail.com>
2018-01-17 07:34:13 -0500

    Update allowed hosts for testing

--- rpg_blog/settings/production.py
+++ rpg_blog/settings/production.py
@@ -2,6 +2,8 @@ from .base import *
 
 ALLOWED_HOSTS = ['www.rpg.nathanhare.net',
     'rpg.nathanhare.net',
+    'rpg2.nathanhare.net',
+    'www.rpg2.nathanhare.net',
 ]
 
 STATIC_ROOT = '/home/web/sites/static/rpg_blog/'
