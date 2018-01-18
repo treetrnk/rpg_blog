b0f708c7397edce5617d2ca080e115be4f8c7491
Nathan Hare <nhare330@gmail.com>
2018-01-18 02:49:34 -0500

    Change static root to universal location

--- rpg_blog/settings/base.py
+++ rpg_blog/settings/base.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "../../data/rpg_blog/static"),]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, '../../data/rpg_blog/media/')
 MEDIA_URL = '/media/'
--- rpg_blog/settings/production.py
+++ rpg_blog/settings/production.py
@@ -6,4 +6,3 @@ ALLOWED_HOSTS = ['www.rpg.nathanhare.net',
     'www.rpg2.nathanhare.net',
 ]
 
-STATIC_ROOT = '/srv/http/rpg_blog/static/'
