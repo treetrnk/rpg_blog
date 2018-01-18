5ae5ff2c2a3f74b40c9677f4699099a057ced49d
Nathan Hare <nhare330@gmail.com>
2018-01-17 20:00:12 -0500

    Update database locations

--- rpg_blog/settings/development.py
+++ rpg_blog/settings/development.py
@@ -2,3 +2,12 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, '../../development/rpg_blog/db.sqlite3'),
+    }
+}
+
+MEDIA_ROOT = os.path.join(BASE_DIR, '../../data/rpg_blog/media/')
