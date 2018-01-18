877394fb704e6fe26d0b3fff4ab3e787c9364cec
Nathan Hare <nhare330@gmail.com>
2018-01-17 20:19:01 -0500

    Clean up db paths

--- rpg_blog/settings/base.py
+++ rpg_blog/settings/base.py
@@ -80,10 +80,9 @@ WSGI_APPLICATION = 'rpg_blog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../../data/rpg_blog/db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
--- rpg_blog/settings/production.py
+++ rpg_blog/settings/production.py
@@ -6,13 +6,6 @@ ALLOWED_HOSTS = ['www.rpg.nathanhare.net',
     'www.rpg2.nathanhare.net',
 ]
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../../../data/rpg_blog/db.sqlite3'),
-    }
-}
-
 STATIC_ROOT = '/srv/http/rpg_blog/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, '../../../data/rpg_blog/media/')
