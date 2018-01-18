50fe428051306d83d4583808e9dad4860100420c
Nathan Hare <nhare330@gmail.com>
2018-01-18 02:54:52 -0500

    Readd static_root variable

--- rpg_blog/settings/base.py
+++ rpg_blog/settings/base.py
@@ -121,7 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "../../data/rpg_blog/static"),]
+STATIC_ROOT = os.path.join(BASE_DIR, "../../data/rpg_blog/static")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, '../../data/rpg_blog/media/')
 MEDIA_URL = '/media/'
