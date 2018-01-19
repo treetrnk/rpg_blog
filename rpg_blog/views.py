5ec110f3b920b93448aeae923aacc8cd30f5d8ed
Nathan Hare <nhare330@gmail.com>
2018-01-19 04:59:17 -0500

    Include metadata in about

--- rpg_blog/views.py
+++ rpg_blog/views.py
@@ -6,4 +6,10 @@ def index(request):
 
 def about(request):
     post = Post.objects.get(slug='about')
-    return render(request, 'about.html', {'about': post}) 
+    meta = {
+        'title': 'rpg stuff - A blog by Nathan Hare',
+        'image': '/static/images/logo.png',
+        'favicon': '/static/images/favicon.png',
+        'description': 'A blog by Nathan Hare about Fate Core and other roleplaying games.',
+    }
+    return render(request, 'about.html', {'about': post, 'meta': meta}) 
