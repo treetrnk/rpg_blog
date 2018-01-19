6a5c847f8eaea2e044ae2d44a7ad95d36ff20c6c
Nathan Hare <nhare330@gmail.com>
2018-01-19 04:31:50 -0500

    Make about page dynamic for easy updating

--- rpg_blog/views.py
+++ rpg_blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from apps.blog.models import Post
 
 def index(request):
     return render(request, 'index.html') 
 
 def about(request):
-    return render(request, 'about.html') 
+    post = Post.objects.filter(published_date__year=2500, slug='about')[0]
+    return render(request, 'about.html', {'about': post}) 
