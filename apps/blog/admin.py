19ad3fca19f0c85d34054b581aa65e2dd10436fb
Nathan Hare <nhare330@gmail.com>
2018-01-19 05:38:37 -0500

    Image path in Post

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -5,9 +5,9 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'banner', 'get_banner', 'user', 'published_date')
     prepopulated_fields = {'slug': ('title',)}
     def get_banner(self, obj):
-        return obj.banner.file
+        return obj.banner_url()
     get_banner.short_description = 'Banner Path'
-    get_banner.admin_order_field = "banner__file"
+    get_banner.admin_order_field = "banner"
 
 class ImageAdmin(admin.ModelAdmin):
     list_display = ('name', 'file')
