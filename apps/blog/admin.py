065a2c6696e17c2b4d834df8f750fd2170e77713
Nathan Hare <nhare330@gmail.com>
2018-01-19 05:18:01 -0500

    Add image link to post list

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from .models import Post,Tag,Image
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'banner', 'user', 'published_date')
+    list_display = ('title', 'slug', 'banner', 'get_banner', 'user', 'published_date')
     prepopulated_fields = {'slug': ('title',)}
+    def get_banner(self, obj):
+        return obj.banner.file
+    get_banner.short_description = 'Banner'
 
 class ImageAdmin(admin.ModelAdmin):
     list_display = ('name', 'file')
