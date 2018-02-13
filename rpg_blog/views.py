1f50a5a722b783da17dae22807ba662e598f217f
Nathan Hare <nhare330@gmail.com>
2018-02-13 02:16:17 -0500

    Add error handling page

--- rpg_blog/views.py
+++ rpg_blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from apps.blog.models import Post
+from apps.blog.models import Post,Tag
 
 def index(request):
     return render(request, 'index.html') 
@@ -12,4 +12,15 @@ def about(request):
         'favicon': '/static/images/favicon.png',
         'description': 'A blog by Nathan Hare about Fate Core and other roleplaying games.',
     }
-    return render(request, 'about.html', {'about': post, 'meta': meta}) 
+    tags = Tag.objects.all().order_by('name')
+    return render(request, 'page.html', {'page': post, 'meta': meta, 'tags': tags}) 
+
+def handler404(request):
+    post = Post.objects.get(slug='404-error')
+    meta = {
+        'title': 'rpg stuff - A blog by Nathan Hare',
+        'image': '/static/images/logo.png',
+        'favicon': '/static/images/favicon.png',
+        'description': 'A blog by Nathan Hare about Fate Core and other roleplaying games.',
+    }
+    return render(request, 'page.html', context={'page': post, 'meta': meta, 'tags': tags}, status=404) 
