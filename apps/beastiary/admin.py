3c24bf7f7f1c52bda1a66d621a72fbe481ccea40
Nathan Hare <nhare330@gmail.com>
2018-11-02 20:51:01 -0400

    Bling the npc admin panel

--- apps/beastiary/admin.py
+++ apps/beastiary/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import NPC
 
+class NPCAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'get_url', 'banner', 'user', 'published_date')
+    prepopulated_fields = {'slug': ('title',)}
+    def get_url(self, obj):
+        return format_html(
+                '<a href="{}" target="post">{}</a>',
+                '/npc/' + obj.title[0] + '/' + obj.slug + '/',
+                'View Post'
+        )
+    get_url.short_description = 'URL'
+
 # Register your models here.
-admin.site.register(NPC)
+admin.site.register(NPC, NPCAdmin)
