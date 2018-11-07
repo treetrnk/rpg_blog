45b40de217f8496b9693334bbc0f4eb3ead7ce32
Nathan Hare <nhare330@gmail.com>
2018-11-07 07:00:16 -0500

    Add tag functionality

--- apps/beastiary/views.py
+++ apps/beastiary/views.py
@@ -4,7 +4,9 @@ from apps.blog.models import Tag
 
 def npcs(request, letter='A', slug=''):
     context = {}
-    context["letter"] = letter.upper()
+    if len(letter) > 1:
+        context["tag"] = letter.lower()
+    context["letter"] = letter.title()
     alphabet = [
         'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R',
         'S','T','U','V','W','X','Y','Z',
@@ -19,7 +21,10 @@ def npcs(request, letter='A', slug=''):
             context["alphabet"] += cletter + " &nbsp;"
     context["alphabet"] += "</h2>"
 
-    npcs = NPC.objects.filter(title__startswith=letter).filter(published=True).order_by("title")
+    if context["tag"]:
+        npcs = NPC.objects.filter(tags__name__in=[letter]).filter(published=True).order_by("title")
+    else: 
+        npcs = NPC.objects.filter(title__startswith=letter).filter(published=True).order_by("title")
     context["npcs"] = npcs
     try:
         context["selected"] = NPC.objects.get(slug=slug)
@@ -41,7 +46,42 @@ def npcs(request, letter='A', slug=''):
     }
     """
     context["tags"] = Tag.objects.all().order_by('name')
-    print(letter)
-    print(npcs)
-    print(context["npcs"])
+    return render(request, 'beastiary/list.html', context)
+
+def tag(request, tag):
+    context = {}
+    context["tag"] = tag.lower()
+    alphabet = [
+        'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R',
+        'S','T','U','V','W','X','Y','Z',
+    ]
+
+    context["alphabet"] = "<h2 class='text-center'>";
+    for cletter in alphabet:
+        exists = NPC.objects.filter(title__startswith=cletter).filter(published=True)
+        if exists: 
+            context["alphabet"] += "<a href='/npc/" + cletter + "'>" + cletter + "</a> &nbsp;"
+        else:
+            context["alphabet"] += cletter + " &nbsp;"
+    context["alphabet"] += "</h2>"
+
+    npcs = NPC.objects.filter(tags__name__in=[tag]).filter(published=True).order_by("title")
+    context["npcs"] = npcs
+    context["selected"] = ''
+
+    context["meta"] = {
+        'title': 'NPCs (' + str(tag) + ') - rpg stuff',
+        'image': '',
+        'favicon': '/static/images/favicon.png',
+        'description': 'farts',
+    }
+    """
+    meta = {
+        'title': npcs.title + ' - rpg stuff',
+        'image': str(npcs.banner_url()),
+        'favicon': '/static/images/favicon.png',
+        'description': npcs.description(),
+    }
+    """
+    context["tags"] = Tag.objects.all().order_by('name')
     return render(request, 'beastiary/list.html', context)
